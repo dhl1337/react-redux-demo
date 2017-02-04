83030bff37e5282560dd43a26af69a8a3d536647
Dan Le <dhl1337@gmail.com>
2017-02-03 22:31:21 -0700

    add state to term

---
+++ src/components/search_bar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''}
+    }
+
+    render() {
+        return (
+            <input type="text" onChange={e => console.log(e.target.value)}/>
+        );
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {API_KEY} from '../config/google-api';
-
+import SearchBar from './components/search_bar';
 
 
 const App = () => {
-    return <div>Hello</div>
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    )
 };
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
