8ff1f3d89bdcb4796050926d1ba2ecaf7477df47
Dan Le <dhl1337@gmail.com>
2017-02-04 00:05:13 -0700

    add video that works, and fix null properties

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-const VideoDetail  = ({video}) => {
+const VideoDetail = ({video}) => {
 
-    const videoId = video.id.video.Id;
+    if (!video) {
+        return <div>Loading...</div>;
+    }
+
+    const videoId = video.id.videoId;
     const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
                 <iframe className="embed-responsive-item" src={url}></iframe>
-                <div className="details">
-                    <div>{video.snippet.title}</div>
-                    <div>{video.snipeet.description}</div>
-                </div>
+            </div>
+            <div className="details">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
             </div>
         </div>
+
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {API_KEY} from '../config/google-api';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 class App extends Component {
     constructor(props) {
@@ -20,6 +21,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList
                     videos={this.state.videos}/>
             </div>
