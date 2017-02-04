37deab7458860b68e703ce3f28b88946e10fbdde
Dan Le <dhl1337@gmail.com>
2017-02-03 23:15:32 -0700

    add constructor on index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {API_KEY} from '../config/google-api';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    )
-};
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, videos => this.setState({videos}));
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
